9b29725d3cb865804530c1ffce10637b2d7bbeca
Dami Lee <50016477+iamdami@users.noreply.github.com>
2021-01-13 09:35:09 +0900

    capitalize_upcase_length_sub.rb

---
+++ String/capitalize_upcase_length_sub.rb
@@ -0,0 +1,8 @@
+puts('hello world'.capitalize())
+# Hello world
+puts('hello world'.upcase())
+# HELLO WORLD
+puts('Hello world'.length())
+# 11
+puts('Hello world'.sub('world', 'programming'))
+# Hello programming
