ef31ccb8bff01a50395e36288b36033115b32741
Dami Lee <50016477+iamdami@users.noreply.github.com>
2021-01-13 11:39:30 +0900

    Create Backslash.rb

---
+++ String/Backslash.rb
@@ -0,0 +1,18 @@
+puts("Cute \"ChaCha\"")
+# Cute "ChaCha"
+
+puts("\\")
+# \
+
+puts("Hello\nworld")
+# Hello
+# world
+
+puts("Hello\t\tworld")
+# Hello world
+
+puts("\a")
+# print system basic sound
+
+puts('Hello\nworld')
+# Hello\nworld
