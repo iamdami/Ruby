2df017f51b5487a202b1d33cf17bc31cb2f4e724
Dami Lee <50016477+iamdami@users.noreply.github.com>
2021-01-13 12:06:50 +0900

    Basics_of_variables.rb

---
+++ String/Basics_of_variables.rb
@@ -0,0 +1,18 @@
+# Adding numbers
+puts(10+5)
+# 15
+
+# Adding strings
+puts("10"+"5")
+# 105
+
+
+# variables
+x=10
+y=5
+puts(x+y)
+# 15
+ 
+title = "Good morning"
+puts("Title is "+title)
+# Title is Good morning
