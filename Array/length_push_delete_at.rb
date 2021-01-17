031ca949b61b7645c48cf6cfa53ee5c7cf2da8a7
Dami Lee <50016477+iamdami@users.noreply.github.com>
2021-01-17 22:34:00 +0900

    Create length_push_delete_at.rb

---
+++ Array/length_push_delete_at.rb
@@ -0,0 +1,11 @@
+# Array length
+al = ['A', 'B', 'C', 'D']
+puts(al.length) # 4
+
+# Add element after array
+al.push('E')
+print(al) # ["A", "B", "C", "D", "E"]
+
+# Delete specific element
+al.delete_at(0)
+print(al) # ["B", "C", "D", "E"]
