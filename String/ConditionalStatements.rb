720c44fa8ba26f5e8fbcbd6b10cad8b5d406d39e
Dami Lee <50016477+iamdami@users.noreply.github.com>
2021-01-17 21:34:57 +0900

    Create ConditionalStatements.rb
    In Ruby, the start of a conditional statement is the if. 
    And the end of a conditional statement is the end.
    
    In Python, the start of a conditional statement is also the if. 
    But the end of a conditional statement is Separated by spaces

---
+++ String/ConditionalStatements.rb
@@ -0,0 +1,33 @@
+if true  
+puts("code1")  # code1
+puts("code2")  # code2
+end
+puts("code3")  # code3
+
+
+input = 11
+real = 11
+if real == input
+puts("Hello!")  # Hello!
+end
+
+
+input = 11
+real = 11
+if real == input
+puts("Hello!")  # Hello!
+else
+puts("Who are you?")
+end
+
+
+input = 33
+real_dami = 11
+real_chacha = "ab"
+if real_dami == input
+puts("Hello dami")
+elsif real_chacha == input
+puts("Hello chacha")
+else
+puts("Who are you?")  # Who are you?
+end
